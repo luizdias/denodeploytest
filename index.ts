df6198870273b1e4d25422d9b7b22211baff515f
Luiz Fernando Dias <luizfad@gmail.com>
2021-12-17 01:17:42 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 console.log("Listening on http://localhost:8000");
 serve((_req) => {
-  return new Response("Hello World!", {
+  let test = { "something": "test" }
+  return new Response(test, {
     headers: { "content-type": "text/plain" },
   });
 });
